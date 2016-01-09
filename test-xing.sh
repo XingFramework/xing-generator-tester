2a0d095439658834362d9b07f79ce1e36881f1c9
Judson <nyarly@gmail.com>
2016-01-08 17:46:19 -0800

    Using bundler, moving tasks into xing-framework

--- test-xing.sh
+++ test-xing.sh
@@ -1,44 +1,50 @@
-#!/bin/bash --login
+#!/bin/bash
+set -e
 
 RUBY_VERSION=${RUBY:="2.2.3"}
+PROJECT_NAME=$1
 
 # Optionally leverage RVM to test in an empty gemset
 if [ "${2}" = "--rvm" ]
 then
   export PATH="$HOME/.rvm/bin:$PATH"
   source ~/.rvm/scripts/rvm
-  rvm use $RUBY
-  rvm gemset create $1
-  rvm use $RUBY@$1
+  rvm use $RUBY_VERSION
+  rvm gemset create $PROJECT_NAME
+  rvm use $RUBY_VERSION@$PROJECT_NAME
 
   # needed for 'xing new' CLI tool, until it is a published gem.
-  gem install trollop caliph
+  # gem install trollop caliph # this shouldn't be needed?
 fi
 
-xing new $1
-echo $RUBY > $1/.ruby-version 
-echo $RUBY > $1/backend/.ruby-version 
-echo $RUBY > $1/frontend/.ruby-version 
+xing new --ruby-version=$RUBY_VERSION $PROJECT_NAME
 
 if [ "${2}" = "--rvm" ]
 then
-  echo $1 > $1/.ruby-gemset
-  echo $1 > $1/backend/.ruby-gemset 
-  echo $1 > $1/frontend/.ruby-gemset
+  echo $PROJECT_NAME > $PROJECT_NAME/.ruby-gemset
+  echo $PROJECT_NAME > $PROJECT_NAME/backend/.ruby-gemset
+  echo $PROJECT_NAME > $PROJECT_NAME/frontend/.ruby-gemset
 fi
 
-cd $1
+
+echo Simulating user\'s first setup in new project
+cd $PROJECT_NAME
 cp backend/config/secrets.yml.example backend/config/secrets.yml
 cp backend/config/database.yml.example backend/config/database.yml
-bundle
-cd frontend && bundle
-cd ../backend  && bundle
+if [ -n $POSTGRES_USER ]; then
+  sed -i "s|# username: postgres|username: ${POSTGRES_USER}|" < backend/config/database.yml.example > backend/config/database.yml
+fi
+sed -i  's/# database: CHANGEME.*/database: xingtest/' backend/config/database.yml
+
+cd backend
+rake db:create
 cd ..
 
-bundle exec rake spec:full
-if [ $? -eq 0 ]
+echo Running full spec suite
+if bundle exec rake spec:full
 then
   printf "\e[0;32m %s \e[0m\n" 'SUCCESS!'
+  echo Proceed to examine result in $PROJECT_NAME
 else
   printf "\e[0;31m %s \e[0m\n" 'FAILURE!'
 fi
