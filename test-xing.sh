6e7925534ab0de2ecc5e60ef7d69e59b82bc935d
Judson <nyarly@gmail.com>
2016-01-12 15:20:18 -0800

    Cleanup

--- test-xing.sh
+++ test-xing.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-RUBY_VERSION=${RUBY:="2.2.3"}
 PROJECT_NAME=$1
 
 # Optionally leverage RVM to test in an empty gemset
-if [ "${2}" = "--rvm" ]
-then
+if [ "${2}" = "--rvm" ]; then
   export PATH="$HOME/.rvm/bin:$PATH"
   source ~/.rvm/scripts/rvm
   rvm use $RUBY_VERSION
@@ -17,10 +15,13 @@ then
   # gem install trollop caliph # this shouldn't be needed?
 fi
 
-xing new --ruby-version=$RUBY_VERSION $PROJECT_NAME
+if [ -n $RUBY_VERSION ]; then
+  version_opt="--ruby-version=$RUBY_VERSION"
+fi
 
-if [ "${2}" = "--rvm" ]
-then
+xing new $version_opt $PROJECT_NAME
+
+if [ "${2}" = "--rvm" ]; then
   echo $PROJECT_NAME > $PROJECT_NAME/.ruby-gemset
   echo $PROJECT_NAME > $PROJECT_NAME/backend/.ruby-gemset
   echo $PROJECT_NAME > $PROJECT_NAME/frontend/.ruby-gemset
@@ -32,7 +33,7 @@ cd $PROJECT_NAME
 cp backend/config/secrets.yml.example backend/config/secrets.yml
 cp backend/config/database.yml.example backend/config/database.yml
 if [ -n $POSTGRES_USER ]; then
-  sed -i "s|# username: postgres|username: ${POSTGRES_USER}|" < backend/config/database.yml.example > backend/config/database.yml
+  sed -i "s|# username: postgres|username: ${POSTGRES_USER}|" backend/config/database.yml
 fi
 sed -i  's/# database: CHANGEME.*/database: xingtest/' backend/config/database.yml
 
@@ -41,8 +42,7 @@ rake db:create
 cd ..
 
 echo Running full spec suite
-if bundle exec rake spec:full
-then
+if bundle exec rake spec:full; then
   printf "\e[0;32m %s \e[0m\n" 'SUCCESS!'
   echo Proceed to examine result in $PROJECT_NAME
 else
